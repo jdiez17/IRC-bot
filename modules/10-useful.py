454867884f0417a41c26fd7d8966cffa64d622fb
Jose Manuel Diez <me@jdiez.me>
2012-03-22 22:31:49 +0100

    Added \x01s to make _cat stop whining.

--- modules/10-useful.py
+++ modules/10-useful.py
@@ -7,7 +7,7 @@ class Useful(Module):
 		
 	def parse_ctcp(self, user, line):
 		if '\x01' + "VERSION" + '\x01' in line:
-			return self.send_raw_message("NOTICE " + self.get_username(user) + " :" + "VERSION " + self.config.get('core', 'version'))
+			return self.send_raw_message("NOTICE " + self.get_username(user) + " :" + '\x01' + "VERSION " + self.config.get('core', 'version') + '\x01')
 			
 	def parse(self, msg, cmd, user, arg):
 		if self.nick in cmd and " o " in msg and "?" in msg:
