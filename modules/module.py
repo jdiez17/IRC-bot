8e998ded4373ad29b5153448b4ede994f8c1096f
Jose Manuel Diez <me@jdiez.me>
2012-06-26 21:07:29 +0000

    move along.

--- modules/module.py
+++ modules/module.py
@@ -108,7 +108,7 @@ class Module(object):
 		if user[:2] == "**":
 			user = user[2:]
 		for admin in self.admins:
-			if admin == user and user != self.nick:
+			if admin in user and user != self.nick:
 				return True
 				
 		return False
